cfc2f9ec0282dfa0226c5e5ecb70da8636c6ce98
Bryan Hodgins <bhodgins89@gmail.com>
2022-03-30 23:24:29 -0400

    Add files via upload

--- BEAR/GP2/bearlySoberBackend.py
+++ BEAR/GP2/bearlySoberBackend.py
@@ -1,119 +1,67 @@
 ##BEGIN CODE
 
 from datetime import datetime
+import json
+import csv          
 
-class Drink:
-        def __init__(self):  
-            self.drinkInfo = {
-                "Name":"Unknown",
-                "QuantityNo" : [0,0,0],
-                "QuantityName": ["unknown","unknown","unknown"]
-                }
-
-
-        def __init__(self, name, ingredientOne, ingredientOneName, ingredientTwo,ingredientTwoName, ingredientThree,ingredientThreeName):
-            self.drinkInfo = {
-                "Name":name,
-                "QuantityNo":[ingredientOne,ingredientTwo,ingredientThree],
-                "QuantityName":[ingredientOneName,ingredientTwoName,ingredientThreeName]               
-                }
-          
-
-class Order:
-    def __init__(self):
-        self.orderInfo = {
-            "Server":"Unknown",
-            "Time":"00:00xx",
-            "Drink":"Unknown"
-        }
-
-    def __init__(self,employee,date,drink):
-        self.orderInfo = {
-            "Server": employee,
-            "Time": date,
-            "Drink" : drink.drinkInfo["Name"]
-            
-            }
-       
-    def make(self,bar,drink):
-
-        if bar.stockCheck(drink) == False:
-            return
-
-        bar.barLog.write("\n Server: ", self.orderInfo["Server"] , " Time: ",self.orderInfo["Time"], "Drink: ", self.orderInfo["Drink"])
-
-        #Subtrack first ingredient, second, etc
-        bar.stock[drink.drinkInfo["QuantityName"][0]] -= drink.drinkInfo["QuantityNo"[0]]
-        bar.stock[drink.drinkInfo["QuantityName"][1]] -= drink.drinkInfo["QuantityNo"[1]]
-        bar.stock[drink.drinkInfo["QuantityName"][2]] -= drink.drinkInfo["QuantityNo"[2]]
-
-
-
-
-
-
-##Main class
 class Bar:
-   
+    employee = ""
+    # pass these variable names to makeOrder() second argument
+    old_fashioned = {'whiskey':2,'bitters':0.1,'sugarCube':1,'orangeTwist':1}
+    margarita = {'tequila':2,'cointreau':1,'limeJuice':1}
+    cosmo = {'gin':1,'campari':1,'sweetVermouth':1}
+    negroni = {'gin':1,'campari':1,'sweetVermouth':1}
+    moscow_mule = {'vodka':2,'gingerBeer':6,'limeJuice':0.5}
+    martini = {'vodka':3,'dryVermouth':0.5,'olive':2}
+    mojito = {'rum':2,'limeJuice':0.75,'simpleSyrup':0.5}    
 
+    #opens the json file and removes the quantity specified in recipe dictionary
+    def removeInventory(self, recipe):
+        inventory = {}
+        with open("/home/bryanh/CSC4110_Project2/BEAR/GP2/inventory.json", 'r') as file:
+            inventory = json.load(file)
+        for i in recipe.keys():
+            inventory[i] = round(inventory[i] - recipe[i], 2)
+        with open("/home/bryanh/CSC4110_Project2/BEAR/GP2/inventory.json", "w") as file:
+            file.write(json.dumps(inventory, indent = 4))
 
-    def __init__(self):
-        self.employee = ""
+    # opens the json file and adds the value parameter to the json entry with the specified key pair
+    def addInventory(self, key, value):
+        with open("/home/bryanh/CSC4110_Project2/BEAR/GP2/inventory.json", 'r') as file:
+            inventory = json.load(file)
+        inventory[key] = inventory[key] + value
+        with open("/home/bryanh/CSC4110_Project2/BEAR/GP2/inventory.json", "w") as file:
+            file.write(json.dumps(inventory, indent = 4))
 
-        self.barLog = open("BarLog.txt",'w')
-        
-        self.drinks = {
-            "Old Fashioned": Drink("Old Fashioned",2,"Whiskey",0.1,"Bitters",1,"Sugar Cube"),
-            "Margarita": Drink("Margarita",2,"Tequila",1,"Cointreau",1,"Lime Juice"),
-            "Cosmo": Drink("Cosmo",1.5,"Vodka",1,"Cointreau",.5,"Lime Juice"),
-            "Negroni": Drink("Negroni",1,"Gin",1,"Campari",1,"Sweet Vermouth"),
-            "Moscow Mule": Drink("Moscow Mule",2,"Vodka",6,"Ginger Beer",.5,"Lime Juice"),
-            "Gin Martini": Drink("Gin Martini",3,"Gin",0.5,"Dry Vermouth",2,"Olive"),
-            "Vodka Martini": Drink("Vodka Martini",3,"Vodka",0.5,"Dry Vermouth",2,"Olive"),
-            "Mojito": Drink("Mojito",2,"Rum",0.75,"Lime Juice",.5,"Simple Syrup")
-             
-            }
-        self.stock = {
-            "Whiskey" : 20.0,
-            "Bitters" : 20.0,
-            "Sugar Cube" : 20.0,
-            "Tequila" : 20.0,
-            "Cointreau" : 20.0,
-            "Lime Juice" : 20.0,
-            "Vodka" : 20.0,
-            "Gin" : 20.0,
-            "Campari" : 20.0,
-            "Sweet Vermouth" : 20.0,
-            "Ginger Beer" : 20.0,
-            "Dry Vermouth": 20.0,
-            "Olive" : 20.0,
-            "Simple Syrup" : 20.0,
-            "Rum" : 20.0
-            }
+    # interacts with the csv file to track server, time, and order that was placed
+    def logOrder(self,drink):
+        with open('/home/bryanh/CSC4110_Project2/BEAR/GP2/barLog.csv', 'a', newline='') as file:
+            writer_object = csv.writer(file)
+            writer_object.writerow([self.employee,datetime.now(),drink])
+            file.close()
 
-
-    def makeOrder(self,bar,drink):
-        newOrder = Order(self.employee,datetime.now(),drink)
-        newOrder.make(self,drink)
-
-
-
-    def logOn(self,employee): #Sets employee thru tkinter button
-        self.employee = employee
-
-    def addInventory(ingredient, quantity): #Add stock (tkinter function)
-        self.stock[ingredient] += quantity
-
-    def stockCheck(self,drink):
-        
-        #Stock -> name of ingredient is used as a key to find corresponding value in stock dictionary, then compared to value in actual drink being ordered
-        if self.stock[drink.drinkInfo["QuantityName"][0]] < drink.drinkInfo["QuantityNo"][0]:
-            print("Not enough ", drink.drinkInfo["QuantityName"][0])
-            return False
-        if self.stock[drink.drinkInfo["QuantityName"][1]] < drink.drinkInfo["QuantityNo"][1]:
-            print("Not enough ", drink.drinkInfo["QuantityName"][1])
-            return False
-        if self.stock[drink.drinkInfo["QuantityName"][2]] < drink.drinkInfo["QuantityNo"][2]:
-            print("Not enough ", drink.drinkInfo["QuantityName"][2])
-            return False
+    #checks the inventory and prints to the console with messages currently
+    def checkInventory(self,recipe):
+        inventory = {}
+        with open("/home/bryanh/CSC4110_Project2/BEAR/GP2/inventory.json", 'r') as file:
+            inventory = json.load(file)
+        for i in recipe.keys():
+            if inventory[i] < recipe[i]:
+                print("Insufficient inventory available!")
+                return False
+            elif inventory[i] < 6 * recipe[i]:
+                print("Low inventory, replenish soon")
         return True
+
+    # pass the name of the drink to the function as a string, and the Bar object dictionary with 
+    # the same key.
+    def makeOrder(self,drink,drinkDict):
+        if self.checkInventory(drinkDict):
+            self.logOrder(drink)
+            self.removeInventory(drinkDict)
+            return True
+        return False
+
+    # trigger off of tkinter button before orders are placed to maintain an open session for the selected server
+    def logOn(self,name): 
+        self.employee = name
